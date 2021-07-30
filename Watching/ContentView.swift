ae5488c50478d6c2700b52ddcfd42c465e7104f6
LuckDuracell <lukedru@yahoo.com>
2021-07-29 23:58:18 -0700

    getting started

--- Watching/ContentView.swift
+++ Watching/ContentView.swift
@@ -8,9 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var shows = Show.loadFromFile()
+    @State var movies = Movie.loadFromFile()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                ZStack {
+                    //Background
+                    VStack {
+                        ForEach(movies, id: \.self, content: { movie in
+                            HStack {
+                                Image(systemName: movie.icon)
+                                    .foregroundColor(.pink)
+                                    .font(.system(size: 20, weight: .medium))
+                                Text(movie.name)
+                                    .font(.system(size: 20, weight: .medium, design: .rounded))
+                                Spacer()
+                                Text("In 3 Days")
+                            } .padding()
+                            .background(Color.gray.opacity(0.2))
+                            .cornerRadius(15)
+                            .padding(.horizontal)
+                        })
+                        ForEach(shows, id: \.self, content: { show in
+                            
+                        })
+                    }
+                }
+                .navigationBarItems(
+                    leading:
+                        Text("Watchable")
+                        .font(.system(size: 33))
+                        .bold(),
+                    trailing:
+                        Button {
+                            movies.append(Movie(name: "Star Wars 10", icon: "tv", releaseDate: Date(), active: false, info: "", platform: "Theater"))
+                        } label: {
+                            Image(systemName: "tv")
+                                .font(Font.title.weight(.medium))
+                                .padding(5)
+                                .foregroundColor(.pink)
+                        }
+                )
+            }
+        } .padding(.top, 30)
     }
 }
 
