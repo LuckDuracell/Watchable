219c49929d8803866cd697d2195ce28c554bb347
LuckDuracell <lukedru@yahoo.com>
2021-07-30 00:30:32 -0700

    decent layout using LazyVGrids and NavigationLinks :)

--- Watching/ContentView.swift
+++ Watching/ContentView.swift
@@ -12,48 +12,121 @@ struct ContentView: View {
     @State var shows = Show.loadFromFile()
     @State var movies = Movie.loadFromFile()
     
+    @State var upcomingMovies: [Movie] = []
+    @State var upcomingShows: [Show] = []
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 ZStack {
                     //Background
                     VStack {
-                        ForEach(movies, id: \.self, content: { movie in
+                        if upcomingMovies.count != 0 || upcomingShows.count != 0 {
                             HStack {
-                                Image(systemName: movie.icon)
-                                    .foregroundColor(.pink)
-                                    .font(.system(size: 20, weight: .medium))
-                                Text(movie.name)
-                                    .font(.system(size: 20, weight: .medium, design: .rounded))
+                                Text("Upcoming:")
+                                    .foregroundColor(.gray)
+                                    .padding(.top, 25)
                                 Spacer()
-                                Text("In 3 Days")
-                            } .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(15)
-                            .padding(.horizontal)
-                        })
-                        ForEach(shows, id: \.self, content: { show in
-                            
-                        })
+                            } .padding(.horizontal)
+                            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], content: {
+                                ForEach(upcomingMovies, id: \.self, content: { movie in
+                                    VStack {
+                                        HStack {
+                                            ZStack {
+                                                Circle()
+                                                    .frame(width: 28, height: 28, alignment: .center)
+                                                    .foregroundColor(.pink)
+                                                Image(systemName: movie.icon)
+                                                    .resizable()
+                                                    .scaledToFill()
+                                                    .frame(width: 12, height: 12)
+                                            } .padding(-5)
+                                            Text(movie.name)
+                                                .fontWeight(.medium)
+                                            Spacer()
+                                        }
+                                        Text("Release: 5 Days")
+                                    }
+                                    .padding()
+                                    .background(.regularMaterial, in: RoundedRectangle(cornerRadius: 16))
+                                })
+                                ForEach(upcomingShows, id: \.self, content: { show in
+                                    Text(show.name)
+                                        .bold()
+                                })
+                            }) .padding()
+                        }
+                        if movies.count != 0 {
+                            HStack {
+                                Text("Watching:")
+                                    .foregroundColor(.gray)
+                                    .padding(.top, 25)
+                                Spacer()
+                            } .padding(.horizontal)
+                            ForEach(movies, id: \.self, content: { movie in
+                                HStack {
+                                    Image(systemName: movie.icon)
+                                        .foregroundColor(.pink)
+                                        .font(.system(size: 20, weight: .medium))
+                                    Text(movie.name)
+                                        .font(.system(size: 20, weight: .medium, design: .rounded))
+                                    Spacer()
+                                    NavigationLink(destination: {
+//                                        var name: String
+//                                        var icon: String
+//                                        var releaseDate: Date
+//                                        var active: Bool
+//                                        var info: String
+//                                        var platform: String
+                                        Text(movie.name)
+                                            .font(.title)
+                                    }, label: {
+                                        Image(systemName: "chevron.right")
+                                            .foregroundColor(.gray)
+                                    })
+                                } .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(15)
+                                .padding(.horizontal)
+                            })
+                        }
+                        if shows.count != 0 {
+                            HStack {
+                                Text("Watching:")
+                                    .foregroundColor(.gray)
+                                    .padding(.top, 25)
+                                Spacer()
+                            } .padding(.horizontal)
+                            ForEach(shows, id: \.self, content: { show in
+                                
+                            })
+                        }
                     }
                 }
+                .navigationTitle("Watchable")
                 .navigationBarItems(
                     leading:
-                        Text("Watchable")
-                        .font(.system(size: 33))
-                        .bold(),
-                    trailing:
                         Button {
-                            movies.append(Movie(name: "Star Wars 10", icon: "tv", releaseDate: Date(), active: false, info: "", platform: "Theater"))
+                            let toDate = Calendar.current.date(byAdding: .day, value: 3, to: Date())!
+                            movies.append(Movie(name: "Iron Man", icon: "tv", releaseDate: Date(), active: false, info: "", platform: "Theater"))
+                            upcomingMovies.append(Movie(name: "Iron Man", icon: "tv", releaseDate: toDate, active: false, info: "", platform: "Theater"))
                         } label: {
                             Image(systemName: "tv")
-                                .font(Font.title.weight(.medium))
+                                .padding(5)
+                                .foregroundColor(.pink)
+                        },
+                    trailing:
+                        Button {
+                            movies.append(Movie(name: "Iron Man", icon: "tv", releaseDate: Date(), active: false, info: "", platform: "Theater"))
+                            upcomingMovies.append(Movie(name: "Iron Man", icon: "tv", releaseDate: Date(), active: false, info: "", platform: "Theater"))
+                        } label: {
+                            Image(systemName: "tv")
                                 .padding(5)
                                 .foregroundColor(.pink)
                         }
                 )
             }
-        } .padding(.top, 30)
+        }
     }
 }
 
