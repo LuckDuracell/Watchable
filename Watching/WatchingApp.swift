ff593823132d7005e712ac616c7b81063e85d3c0
LuckDuracell <lukedru@yahoo.com>
2021-07-29 23:31:19 -0700

    Initial Commit

---
+++ Watching/WatchingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchingApp.swift
+//  Watching
+//
+//  Created by Luke Drushell on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
